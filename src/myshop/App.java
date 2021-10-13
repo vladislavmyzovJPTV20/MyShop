04e5cccae01ee9dc36f3c5c609b75aa781d032fd
pupil <pupil@ivkhk.ee>
2021-10-13 15:20:49 +0300

    Математическая модель продуктового магазина

--- src/myshop/App.java
+++ src/myshop/App.java
@@ -5,6 +5,8 @@
  */
 package myshop;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 import myclasses.Customer;
 import myclasses.History;
@@ -29,6 +31,10 @@ public class App {
             System.out.println("2: Список продуктов");
             System.out.println("3: Добавление покупателя");
             System.out.println("4: Список покупателей");
+            System.out.println("5: Купить продукт");
+            System.out.println("6: Список купленных продуктов");
+            System.out.println("7. Возврат просроченного продукта");
+            System.out.println("8: Расходы");
             
             int task = scanner.nextInt(); scanner.nextLine();
             switch(task) {
@@ -94,5 +100,42 @@ public class App {
         System.out.println("Введите количество денег данного покупателя: ");
         customer.setMoney(scanner.nextDouble()); scanner.nextLine();
         return customer;
+    }
+    
+    private History addHistory() {
+        History history = new History();
+        /**
+         * 1. Вывести пронумерованный список продуктов магазина
+         * 2. Попросить пользователя выбрать номер продукта
+         * 3. Вывести пронумерованный список покупателей
+         * 4. Попросить пользователя выбрать номер покупателя
+         * 5. Сгенерировать текущую дату покупки продукта
+         * 6. Инициировать объект History (задать состояние)
+         */
+        System.out.println("Пронумерованный список продуктов магазина");
+        for (int i = 0; i < products.length; i++) {
+            if(products[i] != null) {
+                System.out.println(i+1+". "+products[i].toString());
+            }
+        }
+        
+        System.out.println("Введите номер продукта: ");
+        int numberProduct = scanner.nextInt(); scanner.nextLine();
+        
+        System.out.println("Пронумерованный список покупателей");
+        for (int i = 0; i < customers.length; i++) {
+            if(customers[i] != null) {
+                System.out.println(i+1+". "+customers[i].toString());
+            }
+        }
+        
+        System.out.println("Введите номер покупателей: ");
+        int numberCustomer = scanner.nextInt(); scanner.nextLine();
+        
+        history.setCustomer(customers[numberCustomer-1]);
+        history.setProduct(products[numberProduct-1]);
+        Calendar c = new GregorianCalendar();
+        history.setPurchaseDate(c.getTime());
+        return history;
     }
 }
