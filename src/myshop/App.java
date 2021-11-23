8b344af524e99a5a8db17015b935a0942d52836e
Vladislav Myzov <mozov.vlad@mail.ru>
2021-11-23 18:39:24 +0200

    Создали математическую модель продуктового магазина с использованием базы данных для хранения информации

--- src/myshop/App.java
+++ src/myshop/App.java
@@ -62,6 +62,9 @@ public class App {
             System.out.println("9: Список категорий товаров");
             System.out.println("10: Выборка продуктов по категории");
             System.out.println("11: Выборка продуктов по какому-то слову");
+            System.out.println("12: Редактирование продукта");
+            System.out.println("13: Редактирование покупателя");
+            System.out.println("14: Редактирование категории");
 
             int task = scanner.nextInt();
             scanner.nextLine();
@@ -114,6 +117,18 @@ public class App {
                 case 11:
                     System.out.println("---- Выборка продуктов по какому-то слову -----");
                     selectionOfProductsByWord();
+                    break;
+                case 12:
+                    System.out.println("---- Редактирование продукта -----");
+                    updateProduct();
+                    break;
+                case 13:
+                    System.out.println("---- Редактирование покупателя -----");
+                    updateCustomer();
+                    break;
+                case 14:
+                    System.out.println("---- Редактирование категории -----");
+                    updateCategory();
                     break;
                 default:
                     System.out.println("Введите номер из списка!");
@@ -419,5 +434,137 @@ public class App {
         if(n == 0) {
             System.out.println("Продуктов, содержащих данные символы - нет!");
         }
+    }
+    
+    private void updateProduct() {
+        Set<Integer> setNumbersProducts = printListAllProducts();
+        if(setNumbersProducts.isEmpty()){
+            System.out.println("Нет продуктов в базе данных!");
+            return;
+        }
+        System.out.println("Выберите номер продукта: ");
+        int numProduct = insertNumber(setNumbersProducts);
+        Set<Integer> setNum = new HashSet<>();
+        setNum.add(1);
+        setNum.add(2);
+        System.out.println("Название продукта: "+products.get(numProduct - 1).getProductname());
+        System.out.println("Хотите изменить нажмите 1, оставить без изменения 2");
+        int change = insertNumber(setNum);
+        if(1 == change){
+            System.out.println("Введите новое название продукта: ");
+            products.get(numProduct - 1).setProductname(scanner.nextLine());
+        }
+        System.out.println("Стоимость обуви: "+products.get(numProduct - 1).getPrice());
+        System.out.println("Хотите изменить нажмите 1, оставить без изменения 2");
+        change = insertNumber(setNum);
+        if(1 == change){
+            System.out.println("Введите новую цену продукта: ");
+            products.get(numProduct - 1).setPrice(scanner.nextDouble());
+        }
+        System.out.println("Количество экземпляров продукта: "+products.get(numProduct - 1).getQuantity());
+        System.out.println("Хотите изменить нажмите 1, оставить без изменения 2");
+        change = insertNumber(setNum);
+        if(1 == change){
+            System.out.println("Введите новое количество продуктов: ");
+            int oldCount = products.get(numProduct - 1).getCount();
+            int oldQuantity = products.get(numProduct - 1).getQuantity();
+            int newQuantity;
+            do {                
+                newQuantity = getNumber();
+                if(newQuantity >= 0 && newQuantity >= oldQuantity - oldCount){
+                    break;
+                }
+                System.out.println("Попробуй еще (>"+(oldQuantity - oldCount)+"): ");
+            } while (true);
+            int newCount = oldCount + (newQuantity - oldQuantity);
+            products.get(numProduct - 1).setQuantity(newQuantity);
+            products.get(numProduct - 1).setCount(newCount);
+        }
+        keeper.saveProducts(products);
+    }
+    
+   private Set<Integer> printListAllProducts() {
+        System.out.println("Список продуктов: ");
+        products = keeper.loadProducts();
+        Set<Integer> setNumbersProducts = new HashSet<>();
+        for (int i = 0; i < products.size(); i++) {
+            StringBuilder cbCategories = new StringBuilder();
+            for (int j = 0; j < products.get(i).getCategory().size(); j++) {
+                cbCategories.append(products.get(i).getCategory().get(j).getCategoryName())
+                        .append(". ");
+            }
+            if(products.get(i) != null && products.get(i).getCount() >= 0){
+                System.out.printf("%d. Продукт: %s. Категория продукта: %s. В наличии экземпляров: %d%n"
+                        ,i+1
+                        ,products.get(i).getProductname()
+                        ,cbCategories.toString()
+                        ,products.get(i).getCount()
+                );
+                setNumbersProducts.add(i+1);
+            }else if(products.get(i) != null){
+                System.out.printf("%d. Продукт %s. Категория продукта: %s. Нет в наличии."
+                        ,i+1
+                        ,products.get(i).getProductname()
+                        ,cbCategories.toString()
+                );
+            }
+        }
+        return setNumbersProducts;        
+    }
+
+    private void updateCustomer() {
+        Set<Integer> setNumbersCustomers = printListCustomers();
+        if(setNumbersCustomers.isEmpty()){
+            System.out.println("Нет покупателей в базе данных!");
+            return;
+        }
+        System.out.println("Выберите номер покупателя: ");
+        int numСustomer = insertNumber(setNumbersCustomers);
+        Set<Integer> setNum = new HashSet<>();
+        setNum.add(1);
+        setNum.add(2);
+        System.out.println("Имя покупателя: "+customers.get(numСustomer - 1).getFirstname());
+        System.out.println("Хотите изменить нажмите 1, оставить без изменения 2");
+        int change = insertNumber(setNum);
+        if(1 == change){
+            System.out.println("Введите новое имя покупателя: ");
+            customers.get(numСustomer - 1).setFirstname(scanner.nextLine());
+        }
+        System.out.println("фамилия покупателя: "+customers.get(numСustomer - 1).getLastname());
+        System.out.println("Хотите изменить нажмите 1, оставить без изменения 2");
+        change = insertNumber(setNum);
+        if(1 == change){
+            System.out.println("Введите новую фамилию покупателя: ");
+            customers.get(numСustomer - 1).setLastname(scanner.nextLine());
+        }
+        System.out.println("Деньги покупателя: "+customers.get(numСustomer - 1).getMoney());
+        System.out.println("Хотите изменить нажмите 1, оставить без изменения 2");
+        change = insertNumber(setNum);
+        if(1 == change){
+            System.out.println("Введите новое количество денег покупателя: ");
+            customers.get(numСustomer - 1).setMoney(scanner.nextDouble());
+        }
+        keeper.saveCustomers(customers);
+    }
+    
+    private void updateCategory() {
+        Set<Integer> setNumbersCategories = printListCategories();
+        if(setNumbersCategories.isEmpty()){
+            System.out.println("Нет категорий в базе");
+            return;
+        }
+        System.out.println("Выберите номер категории: ");
+        int numCategory = insertNumber(setNumbersCategories);
+        Set<Integer> setNum = new HashSet<>();
+        setNum.add(1);
+        setNum.add(2);
+        System.out.println("Название категории: "+categories.get(numCategory - 1).getCategoryName());
+        System.out.println("Хотите изменить нажмите 1, оставить без изменения 2");
+        int change = insertNumber(setNum);
+        if(1 == change){
+            System.out.println("Введите новое название категории: ");
+            categories.get(numCategory - 1).setCategoryName(scanner.nextLine());
+        }
+        keeper.saveCategories(categories);
     }
 }
